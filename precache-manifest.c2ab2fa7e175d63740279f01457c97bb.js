1f778ceb8d92f03fe0e6ff7d4964832ea01c5925
sohail-coder <shaiksohail2332000@gmail.com>
2022-05-19 00:11:37 +0530

    Updates

---
+++ precache-manifest.c2ab2fa7e175d63740279f01457c97bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c43bdcb49923d522e7d",
+    "url": "/react-tutorial/static/css/main.378c4260.chunk.css"
+  },
+  {
+    "revision": "6c43bdcb49923d522e7d",
+    "url": "/react-tutorial/static/js/main.6c43bdcb.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/react-tutorial/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "0ded4357ded4ebb65126",
+    "url": "/react-tutorial/static/js/2.0ded4357.chunk.js"
+  },
+  {
+    "revision": "6873eb981563552bf43f",
+    "url": "/react-tutorial/static/js/runtime~main.6873eb98.js"
+  },
+  {
+    "revision": "4483eab5c7606e6de292f09001c083c6",
+    "url": "/react-tutorial/index.html"
+  }
+];
